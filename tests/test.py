7cbded3fd3361044579273feb66996a840a6cbb6
Prachi Soni <soni.prachi@bcg.com>
2018-04-10 17:02:51 +0000

    Change in unit tests

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,24 @@
-import unittest
+from buzz import generator
 import xmlrunner
+import unittest
 
-def runner(output='python_tests_xml'):
-    return xmlrunner.XMLTestRunner(output=output)
-
-def find_tests():
-    return unittest.TestLoader().discover('python-buzz')
+class Test(unittest.TestCase):
+    def test_sample_single_word(self):
+        l = ('foo', 'bar', 'foobar')
+        word = generator.sample(l)
+        print(word)
+        self.assertIn(word,l)
+    def test_sample_multiple_words(self):
+        l = ('foo', 'bar', 'foobar')
+        words = generator.sample(l, 2)
+        self.assertEqual(len(words),2)
+        self.assertIn(words[0],l)
+        self.assertIn(words[1],l)
+        self.assertIsNot(words[0],words[1])
+    def test_generate_buzz_of_at_least_five_words(self):
+        phrase = generator.generate_buzz()
+        self.assertGreater(len(phrase.split()),5)
 
 if __name__ == '__main__':
-    runner().run(find_tests())
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='./test-reports'))
+
